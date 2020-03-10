efb776c3c5a91a6d652916b174f5d6e792384bb8
Juan <roguedatascience@gmail.com>
2020-03-10 19:18:55 -0400

    Changing the message to a cat.

--- quote_movie.R
+++ quote_movie.R
@@ -1,9 +1,14 @@
-library(tidyverse)
+suppressPackageStartupMessages(
+    library(tidyverse)
+)
 
 suppressMessages(
-    read_csv('https://raw.githubusercontent.com/roguedatascience/movie_quotes/master/movie_quotes.csv')
-) %>%
-    sample_n(1) %>%
-    .$quote %>%
-    str_c('\n\n', ., '\n\n') %>%
-    message()
+    
+    read_csv('https://raw.githubusercontent.com/roguedatascience/movie_quotes/master/movie_quotes.csv') %>%
+        sample_n(1) %>%
+        .$quote %>%
+        str_c('\n\n', ., '\n\n\n') %>%
+        cat()
+    
+)
+
